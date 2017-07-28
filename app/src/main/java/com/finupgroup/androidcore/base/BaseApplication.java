479ac9988f600f0b5fd60c96f24da93b323e2b19
若云 <zyhdvlp@gmail.com>
2017-07-28 17:12:55 +0800

    modify code

--- app/src/main/java/com/finupgroup/androidcore/base/BaseApplication.java
+++ app/src/main/java/com/finupgroup/androidcore/base/BaseApplication.java
@@ -24,29 +24,14 @@ public class BaseApplication extends Application {
         resetDensity();
     }
 
-    private void resetDensity() {
-        Point size = new Point();
-        ((WindowManager) getSystemService(WINDOW_SERVICE)).getDefaultDisplay().getSize(size);
-        getResources().getDisplayMetrics().xdpi = size.x / DESIGN_WIDTH * 72f;
-        KLog.e("zyh", getResources().getDisplayMetrics().toString());
-        //[ (BaseApplication.java:31)#resetDensity ] DisplayMetrics{density=2.0, width=720, height=1280, scaledDensity=2.0, xdpi=69.119995, ydpi=295.563}
-    }
-
-
     /**
+     * [ (BaseApplication.java:31)#resetDensity ] DisplayMetrics{density=2.0, width=720, height=1280, scaledDensity=2.0, xdpi=69.119995, ydpi=295.563}
+     * <p>
      * density:
-     * 显示屏的逻辑密度。这是一个缩放因子
-     * 密度独立像素单位，其中一个DIP是一个像素
-     * 约160dpi屏幕（例如240×320,1.5“×2”屏幕），
-     * 提供系统显示的基线。因此在160dpi屏幕上
-     * 该密度值将为1;在120 dpi屏幕上将是.75;等等
-     * 该值并不完全遵循实际的屏幕尺寸（如由
-     * {@link #xdpi}和{@link #ydpi}，而是用来缩放大小
-     * 基于显示dpi的总体变化的整体UI。对于
-     * 例如，即使宽度为240x320，屏幕的密度将为1
-     * 1.8“，1.3”等。但是，如果屏幕分辨率增加到
-     * 320x480但是屏幕尺寸保持在1.5“x2”，那么密度就是这样
-     * 增加（可能到1.5）。
+     * 显示屏的逻辑密度。这是一个缩放因子密度独立像素单位，其中一个DIP是一个像素约160dpi屏幕（例如240×320,1.5“×2”屏幕），
+     * 提供系统显示的基线。因此在160dpi屏幕上该密度值将为1;在120 dpi屏幕上将是.75;等等该值并不完全遵循实际的屏幕尺寸（如由
+     * {@link #xdpi}和{@link #ydpi}，而是用来缩放大小基于显示dpi的总体变化的整体UI。对于例如，即使宽度为240x320，屏幕的密度将为1
+     * 1.8“，1.3”等。但是，如果屏幕分辨率增加到320x480但是屏幕尺寸保持在1.5“x2”，那么密度就是这样增加（可能到1.5）。
      * <p>
      * <p>
      * widthPixels：
@@ -61,9 +46,21 @@ public class BaseApplication extends Application {
      * 作为{@link #density}，除了它可能被调整为较小
      * 基于字体大小的用户偏好，运行时增量。
      * <p>
-     * <p>
+     * xdpi:
      * X维度中屏幕每英寸的精确物理像素
+     * ydpi:
+     * <p>
+     * Y维度中屏幕每英寸的精确物理像素。
+     * [ (BaseApplication.java:60)#resetDensity ] Point(720, 1280)
      */
+    private void resetDensity() {
+        Point size = new Point();
+        ((WindowManager) getSystemService(WINDOW_SERVICE)).getDefaultDisplay().getSize(size);
+        getResources().getDisplayMetrics().xdpi = size.x / DESIGN_WIDTH * 72f;
+        KLog.e("zyh", getResources().getDisplayMetrics().toString());
+        KLog.e("zyh", size.toString());
+
+    }
 
     @Override
     public void onConfigurationChanged(Configuration newConfig) {
